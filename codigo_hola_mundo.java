43d5ffa8a1099388ad7326e53a1dd1d9a83c9e0b
manuelgorrizabastos <manuel.gorriz@iesabastos.org>
2024-04-26 17:09:02 +0200

    he añadido que tal estas

--- codigo_hola_mundo.java
+++ codigo_hola_mundo.java
@@ -3,8 +3,8 @@
 
 public class HolaMundo {
     public static void main(String[] args) {
-        // Imprimir "Hola Mundo" en la consola
-        System.out.println("Hola Mundo");
+        // Imprimir "Hola Mundo que tal estas" en la consola
+        System.out.println("Hola Mundo que tal estas");
     }
 }
 
