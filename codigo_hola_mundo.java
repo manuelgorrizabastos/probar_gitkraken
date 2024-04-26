b6cf2db1432f7ff2cd183e0d8416b4da852a848e
manuelgorrizabastos <manuel.gorriz@iesabastos.org>
2024-04-26 17:17:15 +0200

    mas cambios y saltos de linea

--- codigo_hola_mundo.java
+++ codigo_hola_mundo.java
@@ -1,12 +1,20 @@
 
 // HolaMundo.java
 
+
+
 public class HolaMundo {
     public static void main(String[] args) {
         // Imprimir "Hola Mundo que tal estas" en la consola
         System.out.println("Hola Mundo que tal estas");
-    }
+    
+
+     }
+
+
+
 }
 
 //esto son unos comentarios para describir lo que hacemos
 // esto son otros comentarios
+// y mas comentarios por aca
